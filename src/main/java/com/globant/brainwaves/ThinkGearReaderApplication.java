545aedff514c516a8903ff7a04cbbbc7b04a0052
ricardo-sanchez <ricardo.sanchez@globant.com>
2020-03-09 19:25:01 -0700

    CoreEngine Integration

--- src/main/java/com/globant/brainwaves/ThinkGearReaderApplication.java
+++ src/main/java/com/globant/brainwaves/ThinkGearReaderApplication.java
@@ -2,10 +2,9 @@ package com.globant.brainwaves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+
 @EnableFeignClients
 @SpringBootApplication
 public class ThinkGearReaderApplication {
